daa973ca07dcda01b0da35b6332bf72e393d6d9d
Aurelian Oancea <oancea@gmail.com>
2020-10-28 15:56:00 +0100

    Added Factory-Bot

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  test "is valid with valid attributes" do
-    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 20, fats: 10, calories: 400, category_id: categories(:one).id)
+
+  test "updating an entry" do
+    entry = create(:entry, meal_type: "Supper", calories: 200)
+
+    assert_equal entry.fats, 20
+
+    entry.fats = 50
+
     assert entry.save
+
+    assert_equal entry.fats, 50
+  end
+
+  test "is valid with valid attributes" do
+    # create => User.new(...).save | create
+    category = create(:category)
+    user = create(:user)
+
+    # build => Entry.new
+    entry = build(:entry,
+                  meal_type: "Breakfast",
+                  calories: 300,
+                  user: user,
+                  category: category)
+
+    assert entry.save, entry.errors.full_messages.join(', ')
   end
 end
